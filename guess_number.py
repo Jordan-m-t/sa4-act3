19c1549e0968235a75babed93851877fc4bc6033
Jordan Thompson <Jordan.t1620@gmail.com>
2024-02-07 14:29:13 -0600

    Refactored code with a while loop in order to keep guessing. Added a quit function

---
+++ guess_number.py
@@ -0,0 +1,22 @@
+number = 10
+
+print("I'm thinking of a number...")
+
+while True:
+    guess_input = input("What number am I thinking of? (Enter 'q' to quit) ")
+    
+    if guess_input.lower() == 'q':
+        print(f"The number was {number}.")
+        break
+    
+    try:
+        guess = int(guess_input)
+    except ValueError:
+        print("Invalid input. Please enter a valid number or 'q' to quit.")
+        continue
+    
+    if guess == number:
+        print("Congrats! You guessed the right number.")
+        break
+    else:
+        print("Too bad! Try again.")
